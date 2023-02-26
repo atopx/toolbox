bdbacc16e3358410c9926054cb08cad1e83f912a
atopx <3940422@qq.com>
2023-02-26 23:13:53 +0800

    fix db logger comment

--- pkg/db.go
+++ pkg/db.go
@@ -41,7 +41,7 @@ func NewDbClient(cfg map[string]interface{}, logger glog.Interface) (*gorm.DB, e
 
 func defaultDbLogger() glog.Interface {
 	return glog.New(
-		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer（日志输出的目标，前缀和日志包含的内容——译者注）
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
 		glog.Config{
 			SlowThreshold:             time.Second, // 慢 SQL 阈值
 			LogLevel:                  glog.Silent, // 日志级别
