4fcca2af5fa01551aa7c463b4ef65e8dd8e26b03
atopx <3940422@qq.com>
2023-03-18 02:10:49 +0800

    优化客户端trace样式

--- starbox/src-tauri/src/staru/trace.rs
+++ starbox/src-tauri/src/staru/trace.rs
@@ -5,122 +5,85 @@ use std::{
     net::TcpStream,
 };
 
+use chrono::DateTime;
+
 use serde_json::Value;
 
-#[derive(serde::Serialize)]
-pub enum LogType {
-    UNKNOWN,
-    DB,
-    TRACK,
-    RUNTIME,
-}
-
-impl LogType {
-    pub fn to_str(&self) -> &str {
-        return match self {
-            Self::UNKNOWN => "UNKNOWN",
-            Self::DB => "DB",
-            Self::TRACK => "TRACK",
-            Self::RUNTIME => "RUNTIME",
-        };
-    }
-
-    pub fn to_string(&self) -> String {
-        return String::from(self.to_str());
-    }
-}
-
-#[derive(serde::Serialize)]
+#[derive(serde::Serialize, Debug)]
 pub struct TraceLog {
-    timestamp: u64,
+    timestamp: i64,
     timestr: String,
     service: String,
-    r#type: LogType,
+    r#type: String,
     level: String,
     infc: String,
+    func: String,
     message: String,
+    title: String,
+}
+
+impl TraceLog {
+    fn new(value: Value) -> Self {
+
+        let timestr = match value["time"].as_str() {
+            Some(v) => String::from(v),
+            None => String::from("null"),
+        };
+        let service = match value["svr"].as_str() {
+            Some(v) => String::from(v),
+            None => String::from("null"),
+        };
+        let level = match value["level"].as_str() {
+            Some(v) => String::from(v),
+            None => String::from("track"),
+        };
+        let infc = match value["infc"].as_str() {
+            Some(v) => String::from(v),
+            None => String::from("null"),
+        };
+        let func = match value["func"].as_str() {
+            Some(v) => String::from(v),
+            None => String::from("null"),
+        };
+        let message = match value["message"].as_str() {
+            Some(v) => String::from(v),
+            None => String::from("null"),
+        };
+        let timestamp = match DateTime::parse_from_rfc3339(&timestr) {
+            Ok(v) => v.timestamp(),
+            Err(_) => 0,
+        };
+
+
+        let r#type: String;
+
+        if level == "null" {
+            r#type = String::from("track");
+        } else if func.contains("MysqlLogger") {
+            r#type = String::from("db");
+        } else {
+            r#type = String::from("runtime");
+        }
+
+        let title = format!("[{type}-{level}] {infc}");
+
+        return Self {
+            timestamp,
+            timestr,
+            service,
+            r#type,
+            level,
+            infc,
+            func,
+            message,
+            title,
+        };
+    }
 }
 
 #[tauri::command]
-pub fn tracelog(id: &str) -> Vec<TraceLog> {
-    let mut result: Vec<TraceLog> = Vec::new();
-    result.push(TraceLog {
-        timestamp: 1,
-        timestr: format!("2023-01-01 16:00:00"),
-        service: format!("Service1"),
-        r#type: LogType::UNKNOWN,
-        level: format!("Level1"),
-        infc: format!("Infc1"),
-        message: format!("message: {id}"),
-    });
-    result.push(TraceLog {
-        timestamp: 2,
-        timestr: format!("2023-01-01 16:00:10"),
-        service: format!("Service2"),
-        r#type: LogType::DB,
-        level: format!("Level2"),
-        infc: format!("Infc13"),
-        message: format!("message: {id}"),
-    });
-    result.push(TraceLog {
-        timestamp: 3,
-        timestr: format!("2023-01-01 16:00:20"),
-        service: format!("Service2"),
-        r#type:LogType::TRACK,
-        level: format!("Level2"),
-        infc: format!("Infc13"),
-        message: format!("message: {id}"),
-    });
-    result.push(TraceLog {
-        timestamp: 4,
-        timestr: format!("2023-01-01 16:00:30"),
-        service: format!("Service2"),
-        r#type: LogType::RUNTIME,
-        level: format!("Level2"),
-        infc: format!("Infc13"),
-        message: format!("message: {id}"),
-    });
-    result.push(TraceLog {
-        timestamp: 5,
-        timestr: format!("2023-01-01 16:00:50"),
-        service: format!("Service2"),
-        r#type: LogType::RUNTIME,
-        level: format!("Level2"),
-        infc: format!("Infc13"),
-        message: format!("message: {id}"),
-    });
-    result.push(TraceLog {
-        timestamp: 6,
-        timestr: format!("2023-01-01 16:00:60"),
-        service: format!("Service2"),
-        r#type: LogType::RUNTIME,
-        level: format!("Level2"),
-        infc: format!("Infc13"),
-        message: format!("message: {id}"),
-    });
-    result.push(TraceLog {
-        timestamp: 7,
-        timestr: format!("2023-01-01 16:00:70"),
-        service: format!("Service2"),
-        r#type: LogType::RUNTIME,
-        level: format!("Level2"),
-        infc: format!("Infc13"),
-        message: format!("message: {id}"),
-    });
-    result.push(TraceLog {
-        timestamp: 8,
-        timestr: format!("2023-01-01 16:00:80"),
-        service: format!("Service2"),
-        r#type: LogType::RUNTIME,
-        level: format!("Level2"),
-        infc: format!("Infc13"),
-        message: format!("message: {id}"),
-    });
-    return result;
-}
-
-fn fetch_log(trace_id: &str) {
-    let tcp = TcpStream::connect("10.0.0.211:22").unwrap();
+pub fn tracelog(host: &str, id: &str) -> Vec<TraceLog> {
+    let tcp = TcpStream::connect(format!("{host}:22")).unwrap();
     let mut sess = Session::new().unwrap();
     sess.set_tcp_stream(tcp);
     sess.handshake().unwrap();
@@ -129,28 +92,30 @@ fn fetch_log(trace_id: &str) {
         .unwrap();
     let mut channel = sess.channel_session().unwrap();
     channel
-        .exec(format!("/bin/grep -h {trace_id} work/star_union*/logs/*.log").as_str())
+        .exec(format!("/bin/grep -h {id} work/star_union*/logs/*.log").as_str())
         .unwrap();
     let reader = BufReader::new(&mut channel);
-
+    let mut result: Vec<TraceLog> = Vec::new();
     for line in reader.lines() {
         match line {
             Ok(data) => {
-                let value: Value =  serde_json::from_str(data.as_str()).unwrap();
-                println!("{:?}", value);
+                let value: Value = serde_json::from_str(data.as_str()).unwrap();
+                result.push(TraceLog::new(value));
             }
             Err(_) => {}
         }
     }
     channel.wait_close().unwrap();
+    return result;
 }
 
 #[cfg(test)]
 mod tests {
-    use super::fetch_log;
+    use super::tracelog;
 
     #[test]
     fn test_fetch_log() {
-        fetch_log("1678762915168580224");
+        let result = tracelog("127.0.0.1", "1678849282905208556");
+        println!("{:#?}", result);
     }
 }
