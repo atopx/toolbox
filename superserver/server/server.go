f1e499d13a49ac8272c916e00692964425b6bfe2
atopx <3940422@qq.com>
2023-03-27 04:27:28 +0800

    add init data todo

--- superserver/server/server.go
+++ superserver/server/server.go
@@ -18,7 +18,6 @@ import (
 func New() *Server {
 	gin.SetMode(viper.GetString("server.mode"))
 	srv := &Server{engine: gin.New(), middle: middleware.New()}
-	srv.listen = fmt.Sprintf("%s:%d", viper.GetString("server.host"), viper.GetInt("server.port"))
 	srv.engine.Use(
 		srv.middle.CorsMiddleware(),
 		srv.middle.RecoverMiddleware(),
@@ -31,79 +30,33 @@ func New() *Server {
 type Server struct {
 	engine *gin.Engine
 	middle *middleware.Middleware
-	listen string
+	server *http.Server
 }
 
-func (srv *Server) Route() {
-
+func (srv *Server) InitData() {
+	// TODO 系统数据初始化
 }
-
-//func (srv *Server) InitData() {
-//	// init roles
-//	tx := srv.db.Begin()
-//	err := role.NewDao(tx).LoadSystemRole()
-//	if err != nil {
-//		tx.Rollback()
-//		panic(fmt.Errorf("init system role failed: %s", err.Error()))
-//	}
-//
-//	err = role.NewDao(tx).LoadDefaultRole()
-//	if err != nil {
-//		tx.Rollback()
-//		panic(fmt.Errorf("init default role failed: %s", err.Error()))
-//	}
-//
-//	// init admin user
-//	err = user.NewDao(srv.db).LoadSystemUser()
-//	if err != nil {
-//		tx.Rollback()
-//		panic(fmt.Errorf("init system user failed: %s", err.Error()))
-//	}
-//
-//	// bind user and role
-//	userRole := user_role.UserRoleRef{RoleId: role.SystemRole.Id, UserId: user.SystemUser.Id}
-//	user_role.NewDao(srv.db).Connection().FirstOrCreate(&userRole)
-//
-//	// init access
-//	var accessList []access.Access
-//	for _, routeInfo := range srv.engine.Routes() {
-//		record := access.Access{
-//			Path:    routeInfo.Path,
-//			Method:  routeInfo.Method,
-//			Handler: routeInfo.Handler,
-//		}
-//		accessList = append(accessList, record)
-//	}
-//	if err = access.NewDao(srv.db).BatchUpsert(accessList); err != nil {
-//		tx.Rollback()
-//		panic(fmt.Errorf("init access failed: %s", err.Error()))
-//	}
-//	tx.Commit()
-//}
 
 // Start 启动api服务
 func (srv *Server) Start() error {
-	// 路由注册
 	api.Register(srv.engine)
-	// 初始化数据
-	//srv.InitData()
-	// http server
-	server := &http.Server{
-		Addr:           srv.listen,
+	srv.InitData()
+	srv.server = &http.Server{
+		Addr:           fmt.Sprintf("%s:%d", viper.GetString("server.host"), viper.GetInt("server.port")),
 		Handler:        srv.engine,
 		ReadTimeout:    60 * time.Second,
 		WriteTimeout:   60 * time.Second,
 		MaxHeaderBytes: 1 << 20,
 	}
-	logger.System("server listen: http://%s", srv.listen)
-	return srv.start(server)
+	logger.System("server listen: http://%s", srv.server.Addr)
+	return srv.loop()
 }
 
 // start 优雅启停
-func (srv *Server) start(server *http.Server) (err error) {
+func (srv *Server) loop() (err error) {
 	errs := make(chan error, 1)
 	go func() {
-		if err = server.ListenAndServe(); err != nil {
+		if err = srv.server.ListenAndServe(); err != nil {
 			if err != nil {
 				errs <- err
 			}
@@ -117,7 +70,7 @@ func (srv *Server) start(server *http.Server) (err error) {
 	case s := <-manager:
 		switch s {
 		case syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT:
-			err = server.Close()
+			err = srv.server.Close()
 		}
 	}
 	return err
